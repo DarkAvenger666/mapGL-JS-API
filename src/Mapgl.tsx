0ee61f2a585020ff25475ec55f395778f1915c1e
Yakov Rudakov <jasha.volk@yandex.ru>
2026-05-16 22:28:31 +0300

    Program adding a new layer to the map with Traffic accident information

--- src/Mapgl.tsx
+++ src/Mapgl.tsx
@@ -7,8 +7,11 @@ import { Directions } from '@2gis/mapgl-directions';
 import { useControlRotateClockwise } from './useControlRotateClockwise';
 import { ControlRotateCounterclockwise } from './ControlRotateConterclockwise';
 import { MapWrapper } from './MapWrapper';
+import { FeatureCollection, Geometry, GeoJsonProperties } from
+    'geojson';
+import geoData from './data/tiumenskaia-oblast.json';
 
-export const MAP_CENTER = [55.31878, 25.23584];
+export const MAP_CENTER = [65.533371, 57.152529];
 
 export default function Mapgl() {
     const { setMapglContext } = useMapglContext();
@@ -26,7 +29,12 @@ export default function Mapgl() {
                 style: '71c58a1a-1c5c-4ca2-9b29-fd11166dbf1b',
             });
 
+            
+
             map.on('click', (e) => console.log(e));
+            map.on('load', () => {
+                console.log(map.getStyle());
+            });
 
             /**
              * Ruler  plugin
@@ -63,6 +71,51 @@ export default function Mapgl() {
                     [55.35242563034581, 25.23925607042088],
                 ],
             });
+
+            const data: FeatureCollection<Geometry, GeoJsonProperties> =
+                geoData as FeatureCollection<Geometry, GeoJsonProperties>;
+
+            const source = new mapgl.GeoJsonSource(map, {
+                data,
+                attributes: {
+                    visible: true, // Уникальное свойство 
+                },
+            });
+
+            // map.loadImage()
+
+            const layer = {
+                id: 'dtp-data-layer1', // ID каждого слоя должен быть уникальным
+                // Фильтрация или выбор данных для этого слоя 
+                filter: [
+                    'all',
+                    [
+                        'match',
+                        ['sourceAttr', 'visible'],
+                        [true],
+                        true, // Значение при совпадении атрибута 'visible' источника со значением 'true' 
+                        false, // Значение при несовпадении 
+                    ]
+                ],
+                // Тип объекта отрисовки 
+                type: 'point',
+                // Стиль объекта отрисовки 
+                style: {
+                    iconImage: ['match', ['get', 'severity'],
+                        ['Легкий'], 'road-accident-slight', 'road-accident'],
+                    iconWidth: 20,
+                    textField: ['get', 'severity'],
+                    textFont: ['Noto_Sans'],
+                    textFontSize: 18,
+                    textColor: '#ffffff',
+                    iconPriority: 103,
+                    textPriority: 100,
+                },
+            };
+
+            map.on('styleload', () => {
+                map?.addLayer(layer);
+            }); 
 
             setMapglContext({
                 mapglInstance: map,
